3bf091080fb9eb46aa958e2adea4bb2ccb44aad8
smile_loopy <smile.loopys2@gmail.com>
2024-10-29 23:26:43 -0700

    LinkedList - Remove Last

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -69,6 +69,24 @@ public class LinkedList {
         length++;
     }
 
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
 }
 
 
--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -25,28 +25,48 @@ public class Main {
 //
 //        */
 
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.makeEmpty();
+//        myLinkedList.append(1);
+//        myLinkedList.append(2);
+//
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();
+//
+//        System.out.println("\nLinked List:");
+//        myLinkedList.printList();
+//
+//        /*
+//       		EXPECTED OUTPUT:
+//        	----------------
+//        	Head: 1
+//        	Tail: 2
+//        	Length: 2
+//
+//        	Linked List:
+//        	1
+//        	2
+//
+//     	*/
+
         LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.makeEmpty();
-        myLinkedList.append(1);
         myLinkedList.append(2);
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeLast());
 
-        System.out.println("\nLinked List:");
-        myLinkedList.printList();
 
-        /*
-       		EXPECTED OUTPUT:
+    	/*
+        	EXPECTED OUTPUT:
         	----------------
-        	Head: 1
-        	Tail: 2
-        	Length: 2
-
-        	Linked List:
-        	1
         	2
+        	1
+        	null
 
      	*/
 
