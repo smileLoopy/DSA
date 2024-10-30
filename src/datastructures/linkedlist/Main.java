4df21b0cb4950a8007d1f544ababe95c3829cf8b
smile_loopy <smile.loopys2@gmail.com>
2024-10-29 23:31:06 -0700

    LinkedList - Prepend

--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -50,25 +50,75 @@ public class Main {
 //
 //     	*/
 
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//
+//        // (2) Items - Returns 2 Node
+//        System.out.println(myLinkedList.removeLast().value);
+//        // (1) Item - Returns 1 Node
+//        System.out.println(myLinkedList.removeLast().value);
+//        // (0) Items - Returns null
+//        System.out.println(myLinkedList.removeLast());
+//
+//
+//    	/*
+//        	EXPECTED OUTPUT:
+//        	----------------
+//        	2
+//        	1
+//        	null
+//
+//     	*/
 
-        // (2) Items - Returns 2 Node
-        System.out.println(myLinkedList.removeLast().value);
-        // (1) Item - Returns 1 Node
-        System.out.println(myLinkedList.removeLast().value);
-        // (0) Items - Returns null
-        System.out.println(myLinkedList.removeLast());
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(3);
+
+        System.out.println("Before prepend():");
+        System.out.println("-----------------");
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+
+        myLinkedList.prepend(1);
+
+        System.out.println("\n\nAfter prepend():");
+        System.out.println("----------------");
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+
+        /*
+            EXPECTED OUTPUT:
+
+            Before prepend():
+            -----------------
+            Head: 2
+            Tail: 3
+            Length: 2
+
+            Linked List:
+            2
+            3
 
 
-    	/*
-        	EXPECTED OUTPUT:
-        	----------------
-        	2
-        	1
-        	null
+            After prepend():
+            ----------------
+            Head: 1
+            Tail: 3
+            Length: 3
 
-     	*/
+            Linked List:
+            1
+            2
+            3
+
+        */
 
     }
 }
