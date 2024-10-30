b347ab7265dee37eb8eeb213ec0f3dc38322551b
smile_loopy <smile.loopys2@gmail.com>
2024-10-30 00:15:41 -0700

    LinkedList - Set

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -120,6 +120,15 @@ public class LinkedList {
         return temp;
     }
 
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
 }
 
 
