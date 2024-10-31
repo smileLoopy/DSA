546769edb07f0f85b6f81ba9cf58629bce877d8d
smile_loopy <smile.loopys2@gmail.com>
2024-10-31 13:07:44 -0700

    DoublyLinkedList - Append

--- src/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -51,4 +51,17 @@ public class DoublyLinkedList {
         System.out.println("Length: " + length);
     }
 
+    public void append (int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
 }
--- src/datastructures/doublylinkedlist/Main.java
+++ src/datastructures/doublylinkedlist/Main.java
@@ -4,7 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+//        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+//
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println("\nDoubly Linked List:");
+//        myDLL.printList();
+//
+//        /*
+//            EXPECTED OUTPUT:
+//            ----------------
+//            Head: 7
+//            Tail: 7
+//            Length: 1
+//
+//            Doubly Linked List:
+//            7
+//
+//        */
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
 
         myDLL.getHead();
         myDLL.getTail();
@@ -14,15 +36,16 @@ public class Main {
         myDLL.printList();
 
         /*
-            EXPECTED OUTPUT:
-            ----------------
-            Head: 7
-            Tail: 7
-            Length: 1
+       		EXPECTED OUTPUT:
+        	----------------
+        	Head: 1
+        	Tail: 2
+        	Length: 2
 
-            Doubly Linked List:
-            7
+        	Doubly Linked List:
+        	1
+        	2
 
-        */
+     	*/
     }
 }
