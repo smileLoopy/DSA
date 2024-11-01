2c5b7a574c8e9b1a8b6d406834b4e95e8e7ac72a
smile_loopy <smile.loopys2@gmail.com>
2024-11-01 15:38:18 -0700

    DoublyLinkedList - Prepend

--- src/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -64,4 +64,17 @@ public class DoublyLinkedList {
         length++;
     }
 
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
 }
--- src/datastructures/doublylinkedlist/Main.java
+++ src/datastructures/doublylinkedlist/Main.java
@@ -25,9 +25,45 @@ public class Main {
 //
 //        */
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(1);
-        myDLL.append(2);
+//        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+//        myDLL.append(2);
+//
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println("\nDoubly Linked List:");
+//        myDLL.printList();
+//
+//        /*
+//       		EXPECTED OUTPUT:
+//        	----------------
+//        	Head: 1
+//        	Tail: 2
+//        	Length: 2
+//
+//        	Doubly Linked List:
+//        	1
+//        	2
+//
+//     	*/
 
+        DoublyLinkedList myDLL = new DoublyLinkedList(2);
+        myDLL.append(3);
+
+        System.out.println("Before prepend():");
+        System.out.println("-----------------");
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        System.out.println("\nDoubly Linked List:");
+        myDLL.printList();
+
+        myDLL.prepend(1);
+
+        System.out.println("\n\nAfter prepend():");
+        System.out.println("----------------");
         myDLL.getHead();
         myDLL.getTail();
         myDLL.getLength();
@@ -36,16 +72,31 @@ public class Main {
         myDLL.printList();
 
         /*
-       		EXPECTED OUTPUT:
-        	----------------
-        	Head: 1
-        	Tail: 2
-        	Length: 2
+            EXPECTED OUTPUT:
 
-        	Doubly Linked List:
-        	1
-        	2
+            Before prepend():
+            -----------------
+            Head: 2
+            Tail: 3
+            Length: 2
 
-     	*/
+            Doubly Linked List:
+            2
+            3
+
+
+            After prepend():
+            ----------------
+            Head: 1
+            Tail: 3
+            Length: 3
+
+            Doubly Linked List:
+            1
+            2
+            3
+
+        */
+
     }
 }
