688805124dd2c19805923332bf545073d26bae69
smile_loopy <smile.loopys2@gmail.com>
2024-11-02 12:11:33 -0700

    DoublyLinkedList - Remove Last

--- src/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -64,6 +64,21 @@ public class DoublyLinkedList {
         length++;
     }
 
+    public Node removeLast() {
+        if(length == 0) return null;
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+
     public void prepend(int value) {
         Node newNode = new Node(value);
         if(length == 0) {
--- src/datastructures/doublylinkedlist/Main.java
+++ src/datastructures/doublylinkedlist/Main.java
@@ -48,6 +48,26 @@ public class Main {
 //
 //     	*/
 
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myDLL.removeLast().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myDLL.removeLast().value);
+        // (0) Items - Returns null
+        System.out.println(myDLL.removeLast());
+
+
+    	/*
+        	EXPECTED OUTPUT:
+        	----------------
+        	2
+        	1
+        	null
+
+     	*/
+
 //        DoublyLinkedList myDLL = new DoublyLinkedList(2);
 //        myDLL.append(3);
 //
@@ -165,54 +185,54 @@ public class Main {
 //
 //        */
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(1);
-        myDLL.append(3);
-
-        System.out.println("DLL before insert():");
-        myDLL.printList();
-
-        myDLL.insert(1, 2);
-
-        System.out.println("\nDLL after insert(2) in middle:");
-        myDLL.printList();
-
-        myDLL.insert(0, 0);
-
-        System.out.println("\nDLL after insert(0) at beginning:");
-        myDLL.printList();
-
-        myDLL.insert(4, 4);
-
-        System.out.println("\nDLL after insert(4) at end:");
-        myDLL.printList();
-
-
-        /*
-            EXPECTED OUTPUT:
-            ----------------
-            DLL before insert():
-            1
-            3
-
-            DLL after insert(2) in middle:
-            1
-            2
-            3
-
-            DLL after insert(0) at beginning:
-            0
-            1
-            2
-            3
-
-            DLL after insert(4) at end:
-            0
-            1
-            2
-            3
-            4
-
-        */
+//        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+//        myDLL.append(3);
+//
+//        System.out.println("DLL before insert():");
+//        myDLL.printList();
+//
+//        myDLL.insert(1, 2);
+//
+//        System.out.println("\nDLL after insert(2) in middle:");
+//        myDLL.printList();
+//
+//        myDLL.insert(0, 0);
+//
+//        System.out.println("\nDLL after insert(0) at beginning:");
+//        myDLL.printList();
+//
+//        myDLL.insert(4, 4);
+//
+//        System.out.println("\nDLL after insert(4) at end:");
+//        myDLL.printList();
+//
+//
+//        /*
+//            EXPECTED OUTPUT:
+//            ----------------
+//            DLL before insert():
+//            1
+//            3
+//
+//            DLL after insert(2) in middle:
+//            1
+//            2
+//            3
+//
+//            DLL after insert(0) at beginning:
+//            0
+//            1
+//            2
+//            3
+//
+//            DLL after insert(4) at end:
+//            0
+//            1
+//            2
+//            3
+//            4
+//
+//        */
 
     }
 }
