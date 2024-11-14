ba0010003c97e1589d9ea5b65a4882c9733fead5
smile_loopy <smile.loopys2@gmail.com>
2024-11-14 11:03:31 -0800

    Stack - Pop

--- src/datastructures/stack/Main.java
+++ src/datastructures/stack/Main.java
@@ -22,46 +22,66 @@ public class Main {
 //
 //        */
 
+//        Stack myStack = new Stack(2);
+//
+//        System.out.println("Before push():");
+//        System.out.println("--------------");
+//        myStack.getTop();
+//        myStack.getHeight();
+//
+//        System.out.println("\nStack:");
+//        myStack.printStack();
+//
+//        myStack.push(1);
+//
+//        System.out.println("\n\nAfter push():");
+//        System.out.println("-------------");
+//        myStack.getTop();
+//        myStack.getHeight();
+//
+//        System.out.println("\nStack:");
+//        myStack.printStack();
+//
+//        /*
+//            EXPECTED OUTPUT:
+//
+//			Before push():
+//			--------------
+//			Top: 2
+//			Height: 1
+//
+//			Stack:
+//			2
+//
+//
+//			After push():
+//			-------------
+//			Top: 1
+//			Height: 2
+//
+//			Stack:
+//			1
+//			2
+//
+//        */
+
         Stack myStack = new Stack(2);
-
-        System.out.println("Before push():");
-        System.out.println("--------------");
-        myStack.getTop();
-        myStack.getHeight();
-
-        System.out.println("\nStack:");
-        myStack.printStack();
-
         myStack.push(1);
 
-        System.out.println("\n\nAfter push():");
-        System.out.println("-------------");
-        myStack.getTop();
-        myStack.getHeight();
+        // (2) Items - Returns 1 Node
+        System.out.println(myStack.pop().value);
+        // (1) Item - Returns 2 Node
+        System.out.println(myStack.pop().value);
+        // (0) Items - Returns null
+        System.out.println(myStack.pop());
 
-        System.out.println("\nStack:");
-        myStack.printStack();
 
         /*
             EXPECTED OUTPUT:
-
-			Before push():
-			--------------
-			Top: 2
-			Height: 1
-
-			Stack:
-			2
-
-
-			After push():
-			-------------
-			Top: 1
-			Height: 2
-
-			Stack:
-			1
-			2
+            ----------------
+            1
+            2
+            null
 
         */
 
