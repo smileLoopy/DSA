6223162fc0ebf7a60376e776a7f5f0d8fd5de210
smile_loopy <smile.loopys2@gmail.com>
2024-11-14 11:04:51 -0800

    Queue - Constructor

---
+++ src/datastructures/queue/Main.java
@@ -0,0 +1,29 @@
+package datastructures.queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Queue myQueue = new Queue(4);
+
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+
+        System.out.println("\nQueue:");
+        myQueue.printQueue();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            First: 4
+            Last: 4
+            Length: 1
+
+            Queue:
+            4
+
+        */
+
+    }
+}
