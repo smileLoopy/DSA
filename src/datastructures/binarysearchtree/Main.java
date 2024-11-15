5238f501537d55cd468bb4afac65608ad47e17af
smile_loopy <smile.loopys2@gmail.com>
2024-11-15 10:33:26 -0800

    BinarySearchTree - Insert

--- src/datastructures/binarysearchtree/Main.java
+++ src/datastructures/binarysearchtree/Main.java
@@ -4,17 +4,49 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        BinarySearchTree myBST = new BinarySearchTree();
+//
+//
+//        // ROOT MUST BE PUBLIC FOR THIS LINE TO WORK
+//        System.out.println("Root: " + myBST.root);
+//
+//
+//        /*
+//            EXPECTED OUTPUT:
+//            ----------------
+//            Root: null
+//
+//        */
+
         BinarySearchTree myBST = new BinarySearchTree();
 
+        myBST.insert(2);
+        myBST.insert(1);
+        myBST.insert(3);
 
-        // ROOT MUST BE PUBLIC FOR THIS LINE TO WORK
-        System.out.println("Root: " + myBST.root);
+        /*
+            THE LINES ABOVE CREATE THIS TREE:
+                         2
+                        / \
+                       1   3
+        */
+
+
+
+        // ROOT MUST BE PUBLIC FOR THESE LINES TO WORK
+        System.out.println("Root: " + myBST.root.value);
+        System.out.println("\nRoot->Left: " + myBST.root.left.value);
+        System.out.println("\nRoot->Right: " + myBST.root.right.value);
 
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            Root: null
+            Root: 2
+
+            Root->Left: 1
+
+            Root->Right: 3
 
         */
 
