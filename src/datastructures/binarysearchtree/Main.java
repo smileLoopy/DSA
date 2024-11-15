1e7d9127a220a0c7b895381ce4fc99b7706d7ac4
smile_loopy <smile.loopys2@gmail.com>
2024-11-15 10:32:43 -0800

    BinarySearchTree - Constructor

---
+++ src/datastructures/binarysearchtree/Main.java
@@ -0,0 +1,22 @@
+package datastructures.binarysearchtree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+
+        // ROOT MUST BE PUBLIC FOR THIS LINE TO WORK
+        System.out.println("Root: " + myBST.root);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Root: null
+
+        */
+
+    }
+}
