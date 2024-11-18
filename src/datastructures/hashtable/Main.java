344a6d2ba1fcb53f3f08e0265eb89df792636adc
smile_loopy <smile.loopys2@gmail.com>
2024-11-18 11:54:31 -0800

    HashTable - Get

--- src/datastructures/hashtable/Main.java
+++ src/datastructures/hashtable/Main.java
@@ -42,31 +42,54 @@ public class Main {
 //
 //        */
 
+//        HashTable myHashTable = new HashTable();
+//
+//        myHashTable.set("nails", 100);
+//        myHashTable.set("tile", 50);
+//        myHashTable.set("lumber", 80);
+//        myHashTable.set("bolts", 200);
+//        myHashTable.set("screws", 140);
+//
+//        myHashTable.printTable();
+//
+//        /*
+//            EXPECTED OUTPUT:
+//            ----------------
+//            0:
+//            1:
+//            2:
+//            3:
+//               {screws= 140}
+//            4:
+//               {bolts= 200}
+//            5:
+//            6:
+//               {nails= 100}
+//               {tile= 50}
+//               {lumber= 80}
+//
+//        */
+
         HashTable myHashTable = new HashTable();
 
         myHashTable.set("nails", 100);
         myHashTable.set("tile", 50);
         myHashTable.set("lumber", 80);
-        myHashTable.set("bolts", 200);
-        myHashTable.set("screws", 140);
 
-        myHashTable.printTable();
+        System.out.println("Lumber:");
+        System.out.println( myHashTable.get("lumber") );
+
+        System.out.println("\nBolts:");
+        System.out.println( myHashTable.get("bolts") );
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            0:
-            1:
-            2:
-            3:
-               {screws= 140}
-            4:
-               {bolts= 200}
-            5:
-            6:
-               {nails= 100}
-               {tile= 50}
-               {lumber= 80}
+            Lumber:
+            80
+
+            Bolts:
+            0
 
         */
 
