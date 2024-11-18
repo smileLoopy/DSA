f6a61f47ef7ae9a1d534ac91c5b37542d103df43
smile_loopy <smile.loopys2@gmail.com>
2024-11-18 14:18:42 -0800

    Graph - AddEdge

--- src/datastructures/graph/Main.java
+++ src/datastructures/graph/Main.java
@@ -4,19 +4,47 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        Graph myGraph = new Graph();
+//
+//        myGraph.addVertex("A");
+//        myGraph.addVertex("B");
+//
+//        System.out.println("\nGraph:");
+//        myGraph.printGraph();
+//
+//        /*
+//            EXPECTED OUTPUT:
+//            ----------------
+//            Graph:
+//            {A=[], B=[]}
+//
+//        */
+
         Graph myGraph = new Graph();
 
         myGraph.addVertex("A");
         myGraph.addVertex("B");
 
-        System.out.println("\nGraph:");
+
+        System.out.println("\nGraph before addEdge():");
         myGraph.printGraph();
+
+
+        myGraph.addEdge("A", "B");
+
+
+        System.out.println("\nGraph after addEdge():");
+        myGraph.printGraph();
+
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            Graph:
+            Graph before addEdge():
             {A=[], B=[]}
+
+            Graph after addEdge():
+            {A=[B], B=[A]}
 
         */
 
