b04e0c297201f2a1d92780d29a60426573169d93
smile_loopy <smile.loopys2@gmail.com>
2024-11-18 14:19:44 -0800

    Graph - RemoveEdge

--- src/datastructures/graph/Main.java
+++ src/datastructures/graph/Main.java
@@ -20,31 +20,61 @@ public class Main {
 //
 //        */
 
+//        Graph myGraph = new Graph();
+//
+//        myGraph.addVertex("A");
+//        myGraph.addVertex("B");
+//
+//
+//        System.out.println("\nGraph before addEdge():");
+//        myGraph.printGraph();
+//
+//
+//        myGraph.addEdge("A", "B");
+//
+//
+//        System.out.println("\nGraph after addEdge():");
+//        myGraph.printGraph();
+//
+//
+//        /*
+//            EXPECTED OUTPUT:
+//            ----------------
+//            Graph before addEdge():
+//            {A=[], B=[]}
+//
+//            Graph after addEdge():
+//            {A=[B], B=[A]}
+//
+//        */
+
         Graph myGraph = new Graph();
 
         myGraph.addVertex("A");
         myGraph.addVertex("B");
-
-
-        System.out.println("\nGraph before addEdge():");
-        myGraph.printGraph();
-
+        myGraph.addVertex("C");
 
         myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
 
 
-        System.out.println("\nGraph after addEdge():");
+        System.out.println("\nGraph before removeEdge():");
         myGraph.printGraph();
 
+        myGraph.removeEdge("A", "B");
+
+        System.out.println("\nGraph after removeEdge():");
+        myGraph.printGraph();
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            Graph before addEdge():
-            {A=[], B=[]}
+            Graph before removeEdge():
+            {A=[B, C], B=[A, C], C=[A, B]}
 
-            Graph after addEdge():
-            {A=[B], B=[A]}
+            Graph after removeEdge():
+            {A=[C], B=[C], C=[A, B]}
 
         */
 
