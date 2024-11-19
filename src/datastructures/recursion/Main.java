008321c011fba56cc21b3dda7155c3cf83d40b4c
smile_loopy <smile.loopys2@gmail.com>
2024-11-18 20:21:24 -0800

    Recursion - Call + Stack

---
+++ src/datastructures/recursion/Main.java
@@ -0,0 +1,22 @@
+package datastructures.recursion;
+
+public class Main {
+
+    public static void methodThree() {
+        System.out.println("Three");
+    }
+
+    public static void methodTwo() {
+        methodThree();
+        System.out.println("Two");
+    }
+
+    public static void methodOne() {
+        methodTwo();
+        System.out.println("One");
+    }
+
+    public static void main(String[] args) {
+        methodOne();
+    }
+}
