bb42f07f93ec9e5f15300bd153b2809a2fc9bc26
smile_loopy <smile.loopys2@gmail.com>
2024-11-18 20:16:07 -0800

    Heaps - MinHeap Insert

--- src/datastructures/heaps/Main.java
+++ src/datastructures/heaps/Main.java
@@ -31,34 +31,58 @@ public class Main {
 //
 //        */
 
-        Heap myHeap = new Heap();
-        myHeap.insert(95);
+//        Heap myHeap = new Heap();
+//        myHeap.insert(95);
+//        myHeap.insert(75);
+//        myHeap.insert(80);
+//        myHeap.insert(55);
+//        myHeap.insert(60);
+//        myHeap.insert(50);
+//        myHeap.insert(65);
+//
+//        System.out.println(myHeap.getHeap());
+//
+//
+//        myHeap.remove();
+//
+//        System.out.println(myHeap.getHeap());
+//
+//
+//        myHeap.remove();
+//
+//        System.out.println(myHeap.getHeap());
+//
+//        /*
+//            EXPECTED OUTPUT:
+//            ----------------
+//            [95, 75, 80, 55, 60, 50, 65]
+//            [80, 75, 65, 55, 60, 50]
+//            [75, 60, 65, 55, 50]
+//
+//        */
+
+        MinHeap myHeap = new MinHeap();
+        myHeap.insert(99);
+        myHeap.insert(72);
+        myHeap.insert(61);
+        myHeap.insert(58);
+
+        System.out.println(myHeap.getHeap());
+
+        myHeap.insert(10);
+
+        System.out.println(myHeap.getHeap());
+
         myHeap.insert(75);
-        myHeap.insert(80);
-        myHeap.insert(55);
-        myHeap.insert(60);
-        myHeap.insert(50);
-        myHeap.insert(65);
-
-        System.out.println(myHeap.getHeap());
-
-
-        myHeap.remove();
-
-        System.out.println(myHeap.getHeap());
-
-
-        myHeap.remove();
 
         System.out.println(myHeap.getHeap());
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            [95, 75, 80, 55, 60, 50, 65]
-            [80, 75, 65, 55, 60, 50]
-            [75, 60, 65, 55, 50]
-
+            [58, 61, 72, 99]
+            [10, 58, 72, 99, 61]
+            [10, 58, 72, 99, 61, 75]
         */
 
     }
