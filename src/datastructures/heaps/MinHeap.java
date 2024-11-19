db6bf35cca718bce7c61eecccea69ac9197b132f
smile_loopy <smile.loopys2@gmail.com>
2024-11-18 20:17:14 -0800

    Heaps - MinHeap Remove

--- src/datastructures/heaps/MinHeap.java
+++ src/datastructures/heaps/MinHeap.java
@@ -42,4 +42,44 @@ public class MinHeap {
             current = parent(current);
         }
     }
+
+
+    private void sinkDown(int index) {
+        int minIndex = index;
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+
+            if (leftIndex < heap.size() && heap.get(leftIndex) < heap.get(minIndex)) {
+                minIndex = leftIndex;
+            }
+
+            if (rightIndex < heap.size() && heap.get(rightIndex) < heap.get(minIndex)) {
+                minIndex = rightIndex;
+            }
+
+            if (minIndex != index) {
+                swap(index, minIndex);
+                index = minIndex;
+            } else {
+                return;
+            }
+        }
+    }
+
+    public Integer remove() {
+        if (heap.size() == 0) {
+            return null;
+        }
+
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+
+        int minValue = heap.get(0);
+        heap.set(0, heap.remove(heap.size() - 1));
+        sinkDown(0);
+
+        return minValue;
+    }
 }
