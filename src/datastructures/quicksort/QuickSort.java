66440ee621dce2720330876074d3f786324eb86b
smile_loopy <smile.loopys2@gmail.com>
2024-11-23 21:25:06 -0800

    Quick Sort - Quick Sort

--- src/datastructures/quicksort/QuickSort.java
+++ src/datastructures/quicksort/QuickSort.java
@@ -23,21 +23,30 @@ public class QuickSort {
         return swapIndex;
     }
 
+    private static void quickSortHelper(int[] array, int left, int right) {
+        if (left < right) {
+            int pivotIndex = pivot(array, left, right);
+            quickSortHelper(array, left, pivotIndex-1);
+            quickSortHelper(array, pivotIndex+1, right);
+        }
+    }
+
+    public static void quickSort(int[] array) {
+        quickSortHelper(array, 0, array.length-1);
+    }
+
     public static void main(String[] args) {
 
         int[] myArray = {4,6,1,7,3,2,5};
 
-        int returnedIndex = pivot(myArray, 0, 6);
-
-        System.out.println( "\nReturned Index: " + returnedIndex);
+        quickSort(myArray);
 
         System.out.println( Arrays.toString( myArray ) );
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            Returned Index: 3
-            [2, 1, 3, 4, 6, 7, 5]
+            [1, 2, 3, 4, 5, 6, 7]
 
          */
 
